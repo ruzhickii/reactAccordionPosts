bce59a990dd97eddd65ce8c0773dc3177df2a680
Alexander Ruzhickii <ruzhickiisasha@gmail.com>
2018-11-09 12:16:32 +0300

    test react accordionPosts project with nice preloader.

---
+++ src/components/Article.js
@@ -0,0 +1,62 @@
+import React, {PureComponent} from "react";
+
+class Article extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 0
+        }
+    }
+
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return this.state.isOpen !== nextState.isOpen;
+    // }
+
+    componentWillMount() {
+        console.log('===', 'Mounting !');
+    }
+
+    /* componentWillReceiveProps(nextProps) {
+         if (nextProps.defaultOpen !== this.props.defaultOpen) this.setState({
+             isOpen: nextProps.defaultOpen
+         })
+        }
+    */
+
+    componentWillUpdate() {
+        console.log('===', 'Will update !');
+    }
+
+    render() {
+        const {article, isOpen, onButtonClick} = this.props;
+        const body = isOpen && <section className='card-text'>{article.text}</section>;
+        return (
+            <div className='card mx-auto' style={{width: '50%'}}>
+                <div className="card-header">
+                    <h2 onClick={this.incrementCounter}>
+                        {article.title}
+                        clicked {this.state.count}
+                        <button onClick={onButtonClick} className='btn btn-primary btn-lg float-right'>
+                            {isOpen ? 'Close' : 'Open'}
+                        </button>
+                    </h2>
+                </div>
+                <div className='card-body'>
+                    <h6 className='card-subtitle text-muted'>
+                        Creation date: {(new Date(article.date)).toDateString()}
+                    </h6>
+                    {body}
+                </div>
+            </div>
+        )
+    }
+
+    incrementCounter = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    };
+}
+
+export default Article
